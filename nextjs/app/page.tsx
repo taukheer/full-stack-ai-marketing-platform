51b9a0bd196ee188e2ab514b5a5efc75326e4b97
taukheer <34509629+taukheer@users.noreply.github.com>
2024-10-02 23:57:14 +0530

    updated files

--- nextjs/app/page.tsx
+++ nextjs/app/page.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <div>
-      <div className="text-2xl bg-red-500 font-bold">Hello</div>
+      {/* <div className="text-2xl bg-red-500 font-bold">Hello</div> */}
       <Button variant="outline">Click me</Button>
     </div>
   );
